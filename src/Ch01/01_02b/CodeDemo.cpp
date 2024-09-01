e6a8e2a84a8faed0ed0583abe716ec15cf8721dd
ANKamath99 <kamathnaishwarya@gmail.com>
2024-09-01 19:26:33 +0000

    hello world

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,10 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World!"<<endl<<endl;
+    return(0);
+}
